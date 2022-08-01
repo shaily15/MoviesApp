3492886f07333592b38416e3d9aca2e5bb0193da
shaily15 <shaily15.sahoo@gmail.com>
2022-08-01 17:21:57 +0530

    Setting up Paging source

--- app/src/main/java/com/example/moviesdbapp/paging/MoviePagingSource.kt
+++ app/src/main/java/com/example/moviesdbapp/paging/MoviePagingSource.kt
@@ -1,4 +1,17 @@
 package com.example.moviesdbapp.paging
 
-class MoviePagingSource {
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.moviesdbapp.data.api.MoviesAPI
+import com.example.moviesdbapp.data.models.Results
+
+class MoviePagingSource(val moviesAPI: MoviesAPI): PagingSource<Int, Results>() {
+    override fun getRefreshKey(state: PagingState<Int, Results>): Int? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Results> {
+        TODO("Not yet implemented")
+    }
+
 }
